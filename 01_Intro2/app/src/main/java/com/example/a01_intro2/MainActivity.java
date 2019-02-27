c0c3f21d36416a705e1516817d9dd1c954f29b8a
Niklas <dev@corusm.de>
2019-02-27 13:15:23 +0100

    Gruppenarbeit Niklas B., Lukas, Ole, Katharina, Pierre

--- 01_Intro2/app/src/main/java/com/example/a01_intro2/MainActivity.java
+++ 01_Intro2/app/src/main/java/com/example/a01_intro2/MainActivity.java
@@ -1,13 +1,14 @@
-package com.example.niklas.vertretungsplanchecker;
+package com.example.a01_intro2;
 
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
@@ -17,11 +18,6 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // View Elemente erstellen
-    TextView planText;
-    TextView planText2;
-
-    // AsyncDownloadTask
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
@@ -49,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
@@ -59,57 +54,31 @@ public class MainActivity extends AppCompatActivity {
                 String today = jsonObject.getString("dayOfHeute");
                 String tommorow = jsonObject.getString("dayOfMorgen");
                 String planHeute = jsonObject.getString("PlanHeute");
-                String planMorgen = jsonObject.getString("PlanMorgen");
 
                 Log.i("heute", today);
                 Log.i("morgen", tommorow);
 
 
-                String out = "";
                 JSONArray jsonArray = new JSONArray(planHeute);
                 for (int i = 0; i<jsonArray.length(); i++) {
                     JSONObject jsonObject1 = jsonArray.getJSONObject(i);
                     Log.i("Lehrer", jsonObject1.getString("Lehrer"));
                     Log.i("Stunde", jsonObject1.getString("Stunde"));
-
-                    out = out + "\n" + jsonObject1.getString("Lehrer") + " - " + jsonObject1.getString("Stunde") + " - " + jsonObject1.getString("Fach");
                 }
-
-
-                // Text1
-                planText = findViewById(R.id.planText);
-                planText.setText(out);
-
-
-                String out2 = "";
-                JSONArray jsonArray2 = new JSONArray(planMorgen);
-                for (int i = 0; i<jsonArray2.length(); i++) {
-                    JSONObject jsonObject2 = jsonArray2.getJSONObject(i);
-                    Log.i("Lehrer", jsonObject2.getString("Lehrer"));
-                    Log.i("Stunde", jsonObject2.getString("Stunde"));
-
-                    out2 = out2 + "\n" + jsonObject2.getString("Lehrer") + " - " + jsonObject2.getString("Stunde") + " - " + jsonObject2.getString("Fach");
-                }
-
-
-                // Text2
-                planText2 = findViewById(R.id.planText2);
-                planText2.setText(out2);
-
-
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+
             Log.i("JSON", s);
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         DownloadTask task = new DownloadTask();
 
